29dfaa1f30459cc2894cb884478adedeb9bb38d6
owobred <29340449+owobred@users.noreply.github.com>
2024-04-26 17:07:03 +0100

    move functions to structs

--- src/lib.rs
+++ src/lib.rs
@@ -18,7 +18,7 @@ async fn main(req: Request, env: Env, _ctx: Context) -> Result<Response> {
 
     let now = time::OffsetDateTime::now_utc();
 
-    let parsed = match parse_url(&url) {
+    let parsed = match DiscordUrl::try_from_full_url(&url) {
         Ok(parsed) => parsed,
         Err(error) => match error {
             InnerError::ParseError(message) => {
@@ -67,6 +67,48 @@ impl ToString for DiscordUrl {
     }
 }
 
+impl DiscordUrl {
+    fn try_from_full_url(url: &Url) -> std::result::Result<DiscordUrl, InnerError> {
+        let Ok(inner_url) = Url::parse(&url.path()[1..]) else {
+            return Err(InnerError::ParseError("failed to parse inner url"));
+        };
+    
+        let mut path = inner_url
+            .path_segments()
+            .ok_or(InnerError::ParseError("missing attachment section"))?
+            .skip(1);
+        let channel_id: u64 = path
+            .next()
+            .ok_or(InnerError::ParseError("missing channel_id section"))?
+            .parse()
+            .map_err(|_| InnerError::ParseError("failed to parse channel_id section"))?;
+        let attachment_id: u64 = path
+            .next()
+            .ok_or(InnerError::ParseError("missing attachment_id section"))?
+            .parse()
+            .map_err(|_| InnerError::ParseError("failed to parse attachment_id section"))?;
+        let filename = path
+            .next()
+            .ok_or(InnerError::ParseError("missing filename section"))?;
+    
+        let expiry_params = match ExpiryParameters::try_from_params_map(
+            url.query_pairs()
+                .map(|(k, v)| (k.to_string(), v.to_string()))
+                .collect(),
+        ) {
+            Some(params) => Some(params?),
+            None => None,
+        };
+    
+        Ok(DiscordUrl {
+            channel_id,
+            attachment_id,
+            filename: filename.to_string(),
+            expiry_params,
+        })
+    }
+}
+
 #[derive(Debug)]
 struct ExpiryParameters {
     expiry: time::OffsetDateTime,
@@ -74,83 +116,43 @@ struct ExpiryParameters {
     hm: Vec<u8>,
 }
 
-fn parse_url(url: &Url) -> std::result::Result<DiscordUrl, InnerError> {
-    let Ok(inner_url) = Url::parse(&url.path()[1..]) else {
-        return Err(InnerError::ParseError("failed to parse inner url"));
-    };
+impl ExpiryParameters {
+    fn try_from_params_map(params: HashMap<String, String>) -> Option<std::result::Result<Self, InnerError>> {
+        let expiry = match i64::from_str_radix(&params.get("ex")?, 16)
+            .map_err(|_| InnerError::ParseError("failed to parse ex parameter"))
+            .and_then(|val| {
+                time::OffsetDateTime::from_unix_timestamp(val)
+                    .map_err(|_| InnerError::ParseError("failed to parse ex timestamp"))
+            }) {
+            Ok(v) => v,
+            Err(e) => return Some(Err(e)),
+        };
 
-    let mut path = inner_url
-        .path_segments()
-        .ok_or(InnerError::ParseError("missing attachment section"))?
-        .skip(1);
-    let channel_id: u64 = path
-        .next()
-        .ok_or(InnerError::ParseError("missing channel_id section"))?
-        .parse()
-        .map_err(|_| InnerError::ParseError("failed to parse channel_id section"))?;
-    let attachment_id: u64 = path
-        .next()
-        .ok_or(InnerError::ParseError("missing attachment_id section"))?
-        .parse()
-        .map_err(|_| InnerError::ParseError("failed to parse attachment_id section"))?;
-    let filename = path
-        .next()
-        .ok_or(InnerError::ParseError("missing filename section"))?;
+        let is = match i64::from_str_radix(&params.get("is")?, 16)
+            .map_err(|_| InnerError::ParseError("failed to parse is parameter"))
+            .and_then(|val| {
+                time::OffsetDateTime::from_unix_timestamp(val)
+                    .map_err(|_| InnerError::ParseError("failed to parse is timestamp"))
+            }) {
+            Ok(v) => v,
+            Err(e) => return Some(Err(e)),
+        };
 
-    let expiry_params = match parse_expiry_params(
-        url.query_pairs()
-            .map(|(k, v)| (k.to_string(), v.to_string()))
-            .collect(),
-    ) {
-        Some(params) => Some(params?),
-        None => None,
-    };
+        let hm = match params
+            .get("hm")?
+            .to_string()
+            .chars()
+            .array_chunks::<2>()
+            .map(|v| u8::from_str_radix(&v.into_iter().collect::<String>(), 16))
+            .map(|v| v.map_err(|_| InnerError::ParseError("failed to parse hm byte")))
+            .collect::<std::result::Result<Vec<_>, _>>()
+        {
+            Ok(hm) => hm,
+            Err(err) => return Some(Err(err)),
+        };
 
-    Ok(DiscordUrl {
-        channel_id,
-        attachment_id,
-        filename: filename.to_string(),
-        expiry_params,
-    })
-}
-
-fn parse_expiry_params(
-    params: HashMap<String, String>,
-) -> Option<std::result::Result<ExpiryParameters, InnerError>> {
-    let expiry = match i64::from_str_radix(&params.get("ex")?, 16)
-        .map_err(|_| InnerError::ParseError("failed to parse ex parameter"))
-        .and_then(|val| {
-            time::OffsetDateTime::from_unix_timestamp(val)
-                .map_err(|_| InnerError::ParseError("failed to parse ex timestamp"))
-        }) {
-        Ok(v) => v,
-        Err(e) => return Some(Err(e)),
-    };
-
-    let is = match i64::from_str_radix(&params.get("is")?, 16)
-        .map_err(|_| InnerError::ParseError("failed to parse is parameter"))
-        .and_then(|val| {
-            time::OffsetDateTime::from_unix_timestamp(val)
-                .map_err(|_| InnerError::ParseError("failed to parse is timestamp"))
-        }) {
-        Ok(v) => v,
-        Err(e) => return Some(Err(e)),
-    };
-
-    let hm = match params
-        .get("hm")?
-        .to_string()
-        .chars()
-        .array_chunks::<2>()
-        .map(|v| u8::from_str_radix(&v.into_iter().collect::<String>(), 16))
-        .map(|v| v.map_err(|_| InnerError::ParseError("failed to parse hm byte")))
-        .collect::<std::result::Result<Vec<_>, _>>()
-    {
-        Ok(hm) => hm,
-        Err(err) => return Some(Err(err)),
-    };
-
-    Some(Ok(ExpiryParameters { expiry, is, hm }))
+        Some(Ok(ExpiryParameters { expiry, is, hm }))
+    }
 }
 
 #[derive(Debug, thiserror::Error)]
